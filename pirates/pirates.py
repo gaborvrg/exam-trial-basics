3b39a3827d492115e1885508f526b68ad9b1f6ad
Gabor V <gabor.w.varga@gmail.com>
2017-04-10 10:28:17 +0200

    added append method to the app

--- pirates/pirates.py
+++ pirates/pirates.py
@@ -11,15 +11,14 @@ pirates = [
 # - have wooden leg and
 # - have more than 15 gold
 
-def pirate_list(lst):
+def pirate_list(lst):   # open a new function with lst arg.
 
-
+    dict_list = []      # open an empty list
     
-    for dct in lst:
-        if dct['has_wooden_leg'] is True and dct['gold'] > 15:
-            print(dct)
+    for dct in lst:     # check every dicts(elements in the 'pirates' list) 
+        if dct['has_wooden_leg'] is True and dct['gold'] > 15: # if statments
+            dict_list.append(dct['Name'])   # append value of 'Name' keys to the empty list
+    return dict_list    # return the correct answer
 
-    # return
 
-
-pirate_list(pirates)
+print(pirate_list(pirates))
