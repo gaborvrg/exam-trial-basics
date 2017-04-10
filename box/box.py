75860d9f0e535a646434c5dee4ea3828e29902bf
Gabor V <gabor.w.varga@gmail.com>
2017-04-10 10:56:10 +0200

    update comments

--- box/box.py
+++ box/box.py
@@ -4,36 +4,28 @@ class Cuboid():
     # It should take its three dimensions as constructor parameters (numbers)
     # It should have a method called `get_surface` that returns the cuboid's surface
     # It should have a method called `get_volume` that returns the cuboid's volume
+
     def __init__(self,x,y,z):
+        """ Initialize base arguments for the Cuboid class"""
 
         self.x = x
         self.y = y
         self.z = z
 
-
-
     def get_surface(self):
-        # surface = (2*self.x*self.z) + (2*self.z*self.y) + (2*self.y*self.x)
+        """ Count the surface of cubiod"""
         surface = 2 * (self.x*self.y + self.x*self.z + self.y*self.z)
 
         return surface
 
     def get_volume(self):
-
+        """ Count the volume of cubiod"""
         volume = self.x * self.y * self.z
 
         return volume
 
 
-
-
-
-
 box = Cuboid(10, 20, 30)
-
 print(box.get_surface()) # should print 2200
 print(box.get_volume()) # should print 6000
 
-
-# A = 2wl + 2lh + 2hw.  w=x h=y l=z 
-# V = l × w × h
