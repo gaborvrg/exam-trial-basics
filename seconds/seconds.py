6140901e922bf68bb7a0fe6c7ef9d1e4f6f655a1
Gabor V <gabor.w.varga@gmail.com>
2017-04-10 09:23:27 +0200

    added solution to seconds.py with a list slice approach

--- seconds/seconds.py
+++ seconds/seconds.py
@@ -1,7 +1,13 @@
-def seconds(change_this):
-    pass
+def seconds(elements):
+    # pass
     # Create a function that takes a list as a parameter,
     # and returns a new list with every second element from the orignal list
     # example: [1, 2, 3, 4, 5] should produce [2, 4]
+
+
+    # This is an extended slice method. 
+    # It works on every second elements on the list from 0 index to end index.
+
+    return elements[::2] 
 
 print(seconds([1, 2, 3, 4, 5])) # should print [2, 4]
